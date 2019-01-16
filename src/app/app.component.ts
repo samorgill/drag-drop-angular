ad5448bec148be62d6b00a36b04340a543dcbe75
Sam Orgill <ssorgill@icloud.com>
2019-01-16 12:24:33 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dragdrop';
+  title = 'Drag & Drop in Angular 7';
+  name = 'Sam Orgill';
+  website = 'https://samorgill.com';
+
+  todoList = [
+    {
+      name: 'Angular',
+      category: 'Web Development'
+    },
+    {
+      name: 'Flexbox',
+      category: 'Web Development'
+    },
+    {
+      name: 'iOS',
+      category: 'App Development'
+    },
+    {
+      name: 'Java',
+      category: 'Software development'
+    }
+  ];
+
+  doneList = [
+    {
+      name: 'Android',
+      category: 'Mobile Development'
+    },
+    {
+      name: 'MongoDB',
+      category: 'Databases'
+    },
+    {
+      name: 'ARKit',
+      category: 'Augmented Reality'
+    },
+    {
+      name: 'React',
+      category: 'Web Development'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
